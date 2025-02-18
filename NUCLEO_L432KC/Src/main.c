b04c2904e291ce3a9bc174660f34b7af5e7de2df
Pavel Polach <pavel.polach@tropicsquare.com>
2025-02-18 15:32:24 +0100

    feat (./) : Add example implementation for NUCLEO L432KC

---
+++ NUCLEO_L432KC/Src/main.c
@@ -0,0 +1,408 @@
+/**
+  ******************************************************************************
+  * @file    SPI/SPI_FullDuplex_ComPolling/Src/main.c
+  * @author  MCD Application Team
+  * @brief   This sample code shows how to use STM32L4xx SPI HAL API to transmit
+  *          and receive a data buffer with a communication process based on
+  *          Polling transfer.
+  *          The communication is done using 2 Boards.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdio.h>
+#include "libtropic_examples.h"
+
+/** @addtogroup STM32L4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup SPI_FullDuplex_ComPolling
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void Error_Handler(void);
+
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+  * @brief UART MSP De-Initialization
+  *        This function frees the hardware resources used in this example:
+  *          - Disable the Peripheral's clock
+  *          - Revert GPIO and NVIC configuration to their default state
+  * @param huart: UART handle pointer
+  * @retval None
+  */
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
+{
+    USART_DBG_FORCE_RESET();
+    USART_DBG_RELEASE_RESET();
+    HAL_GPIO_DeInit(USART_DBG_TX_GPIO_PORT, USART_DBG_TX_PIN);
+    HAL_GPIO_DeInit(USART_DBG_RX_GPIO_PORT, USART_DBG_RX_PIN);
+}
+
+
+/**
+  * @brief SPI MSP Initialization
+  *        This function configures the hardware resources used in this example:
+  *           - Peripheral's clock enable
+  *           - Peripheral's GPIO Configuration
+  * @param hspi: SPI handle pointer
+  * @retval None
+  */
+void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct ={0};
+
+  // Enable peripherals and GPIO Clocks
+  // Enable GPIO TX/RX clock
+  SPIx_SCK_GPIO_CLK_ENABLE();
+  SPIx_MISO_GPIO_CLK_ENABLE();
+  SPIx_MOSI_GPIO_CLK_ENABLE();
+  /* Enable SPI clock */
+  SPIx_CLK_ENABLE();
+
+  // Configure peripheral GPIO
+  // SPI SCK GPIO pin configuration
+  GPIO_InitStruct.Pin       = SPIx_SCK_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = SPIx_SCK_AF;
+  HAL_GPIO_Init(SPIx_SCK_GPIO_PORT, &GPIO_InitStruct);
+
+  // SPI MISO GPIO pin configuration
+  GPIO_InitStruct.Pin = SPIx_MISO_PIN;
+  GPIO_InitStruct.Alternate = SPIx_MISO_AF;
+  HAL_GPIO_Init(SPIx_MISO_GPIO_PORT, &GPIO_InitStruct);
+
+  // SPI MOSI GPIO pin configuration
+  GPIO_InitStruct.Pin = SPIx_MOSI_PIN;
+  GPIO_InitStruct.Alternate = SPIx_MOSI_AF;
+  HAL_GPIO_Init(SPIx_MOSI_GPIO_PORT, &GPIO_InitStruct);
+}
+
+/**
+  * @brief SPI MSP De-Initialization
+  *        This function frees the hardware resources used in this example:
+  *          - Disable the Peripheral's clock
+  *          - Revert GPIO configuration to its default state
+  * @param hspi: SPI handle pointer
+  * @retval None
+  */
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef *hspi)
+{
+  if(hspi->Instance == SPIx)
+  {
+    // Reset peripherals
+    SPIx_FORCE_RESET();
+    SPIx_RELEASE_RESET();
+
+    // Disable peripherals and GPIO Clocks
+    // Configure SPI SCK as alternate function
+    HAL_GPIO_DeInit(SPIx_SCK_GPIO_PORT, SPIx_SCK_PIN);
+    // Configure SPI MISO as alternate function
+    HAL_GPIO_DeInit(SPIx_MISO_GPIO_PORT, SPIx_MISO_PIN);
+    // Configure SPI MOSI as alternate function
+    HAL_GPIO_DeInit(SPIx_MOSI_GPIO_PORT, SPIx_MOSI_PIN);
+  }
+}
+
+/**
+  * @brief RNG MSP Initialization
+  *        This function inicializes hardware resources used in this example:
+  *          - Enable the Peripheral's clock
+  * @param hrng: RNG handle pointer
+  * @retval None
+  */
+void HAL_RNG_MspInit(RNG_HandleTypeDef *hrng)
+{
+  // RNG Peripheral clock enable
+  __HAL_RCC_RNG_CLK_ENABLE();
+}
+
+/**
+  * @brief RNG MSP De-Initialization
+  *        This function freeze the hardware resources used in this example:
+  *          - Disable the Peripheral's clock
+  * @param hrng: RNG handle pointer
+  * @retval None
+  */
+void HAL_RNG_MspDeInit(RNG_HandleTypeDef *hrng)
+{
+  // Enable RNG reset state
+  __HAL_RCC_RNG_FORCE_RESET();
+
+  // Release RNG from reset state
+  __HAL_RCC_RNG_RELEASE_RESET();
+}
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+/* Private functions ---------------------------------------------------------*/
+
+/* DEBUG UART handle declaration */
+static UART_HandleTypeDef UartHandle;
+
+/**
+ * @brief   Configures the DEBUG UART peripheral
+ *          Put the USART peripheral in the Asynchronous mode (UART Mode)
+ *          UART configured as follows:
+ *          - Word Length = 8 Bits (7 data bit + 1 parity bit) :
+ *                        BE CAREFUL : Program 7 data bits + 1 parity bit in PC HyperTerminal
+ *          - Stop Bit    = One Stop bit
+ *          - Parity      = NONE parity
+ *          - BaudRate    = 115200 baud
+ *          - Hardware flow control disabled (RTS and CTS signals)
+ *
+ * @return HAL_StatusTypeDef
+ */
+static HAL_StatusTypeDef DBG_UART_Init(void)
+{
+
+  GPIO_InitTypeDef  GPIO_InitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  // Enable the HSI clock
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    // Error
+    while(1);
+  }
+
+  // Configure HSI as USART clock source
+  USARTx_RCC_CONFIG(RCC_USARTxCLKSOURCE_HSI);
+
+  // Enable peripherals and GPIO Clocks
+  // Enable GPIO TX/RX clock
+  USART_DBG_TX_GPIO_CLK_ENABLE();
+  USART_DBG_RX_GPIO_CLK_ENABLE();
+  // Enable USART_DBG clock
+  USART_DBG_CLK_ENABLE();
+
+  // Configure UART pins
+  GPIO_InitStruct.Pin       = USART_DBG_TX_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_PULLUP;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = USART_DBG_TX_AF;
+  HAL_GPIO_Init(USART_DBG_TX_GPIO_PORT, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = USART_DBG_RX_PIN;
+  GPIO_InitStruct.Alternate = USART_DBG_RX_AF;
+  HAL_GPIO_Init(USART_DBG_RX_GPIO_PORT, &GPIO_InitStruct);
+
+
+  UartHandle.Instance        = USART2;
+  UartHandle.Init.BaudRate   = 115200;
+  UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+  UartHandle.Init.StopBits   = UART_STOPBITS_1;
+  UartHandle.Init.Parity     = UART_PARITY_NONE;
+  UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+  UartHandle.Init.Mode       = UART_MODE_TX_RX;
+  UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+
+  if (HAL_UART_Init(&UartHandle) != HAL_OK)
+  {
+    return HAL_ERROR;
+  }
+
+  return HAL_OK;
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART3 and Loop until the end of transmission */
+  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
+
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+#ifdef MASTER_BOARD
+  GPIO_InitTypeDef  GPIO_InitStruct;
+#endif
+
+  /* STM32L4xx HAL library initialization:
+       - Configure the Flash prefetch
+       - Systick timer is configured by default as source of time base, but user
+         can eventually implement his proper time base source (a general purpose
+         timer for example or other time source), keeping in mind that Time base
+         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
+         handled in milliseconds basis.
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the system clock to 80 MHz */
+  SystemClock_Config();
+
+  /* Configure LED3 */
+  BSP_LED_Init(LED3);
+
+  if(DBG_UART_Init() != HAL_OK) {
+    Error_Handler();
+  }
+
+  // libtropic related code BEGIN
+  // libtropic related code BEGIN
+  // libtropic related code BEGIN
+  // libtropic related code BEGIN
+  // libtropic related code BEGIN
+
+   // Test routines
+#ifdef LT_EX_TEST_REVERSIBLE
+    lt_ex_test_reversible();
+#endif
+#ifdef LT_EX_TEST_IREVERSIBLE
+    lt_ex_test_ireversible();
+#endif
+
+    // Full examples
+#ifdef LT_EX_FW_UPDATE
+    lt_ex_fw_update();
+#endif
+#ifdef LT_EX_HELLO_WORLD
+    /*int*/ lt_ex_hello_world();
+#endif
+#ifdef LT_EX_HW_WALLET
+    /*int*/ lt_ex_hardware_wallet();
+#endif
+
+  // libtropic related code END
+  // libtropic related code END
+  // libtropic related code END
+  // libtropic related code END
+  // libtropic related code END
+
+  /* Infinite loop */
+  while (1)
+  {
+    //printf("hello\r\n");
+    BSP_LED_Toggle(LED3);
+    HAL_Delay(200);
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  while(1)
+  {
+    /* Toggle LED3 for error */
+    BSP_LED_Toggle(LED3);
+    HAL_Delay(1000);
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follows :
+  *            System Clock source            = PLL (MSI)
+  *            SYSCLK(Hz)                     = 80000000
+  *            HCLK(Hz)                       = 80000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 1
+  *            MSI Frequency(Hz)              = 4000000
+  *            PLL_M                          = 1
+  *            PLL_N                          = 40
+  *            PLL_R                          = 2
+  *            PLL_P                          = 7
+  *            PLL_Q                          = 4
+  *            Flash Latency(WS)              = 4
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* MSI is enabled after System reset, activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLP = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+}
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
