e05a3ce4bcf3c5c140a2d9e39db183087af38834
Pavel Polach <pavel.polach@tropicsquare.com>
2025-03-10 16:22:18 +0100

    feat (./) : Add support for arduino shield

--- NUCLEO_F439ZI/Inc/main.h
+++ NUCLEO_F439ZI/Inc/main.h
@@ -34,7 +34,10 @@
  * if == 1, debug UART can be read when board is plugged over USB (together with stlink)
  * if == 0, debug uart can be read over pins connected to FT2232h, used on automatized testing rig
 */
-#define DBG_UART_OVER_USB 1
+#define DBG_UART_OVER_USB 0
+
+// If 0, SPI is wired on arduino shield pins
+#define LIBTROPIC_TESTS 1
 
 /* Definition for USART_DBG clock resources */
 #define USART_DBG                           USART3
@@ -104,29 +107,53 @@
 #define UART_MODEL_RX_AF                     GPIO_AF8_UART5
 #endif
 #ifdef USE_SPI
-/* User can use this section to tailor SPIx instance used and associated
-   resources */
-/* Definition for SPIx clock resources */
-#define SPIx                             SPI4
-#define SPIx_CLK_ENABLE()                __HAL_RCC_SPI4_CLK_ENABLE()
-#define SPIx_SCK_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOE_CLK_ENABLE()
-#define SPIx_MISO_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOE_CLK_ENABLE()
-#define SPIx_MOSI_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOE_CLK_ENABLE()
-#define SPIx_NSS_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOE_CLK_ENABLE()
-
-#define SPIx_FORCE_RESET()               __HAL_RCC_SPI4_FORCE_RESET()
-#define SPIx_RELEASE_RESET()             __HAL_RCC_SPI4_RELEASE_RESET()
-
-/* Definition for SPIx Pins */
-#define SPIx_SCK_PIN                     GPIO_PIN_2
-#define SPIx_SCK_GPIO_PORT               GPIOE
-#define SPIx_SCK_AF                      GPIO_AF5_SPI4
-#define SPIx_MISO_PIN                    GPIO_PIN_5
-#define SPIx_MISO_GPIO_PORT              GPIOE
-#define SPIx_MISO_AF                     GPIO_AF5_SPI4
-#define SPIx_MOSI_PIN                    GPIO_PIN_6
-#define SPIx_MOSI_GPIO_PORT              GPIOE
-#define SPIx_MOSI_AF                     GPIO_AF5_SPI4
+  #if LIBTROPIC_TESTS
+    #define LT_SPI_CS_BANK   GPIOE
+    #define LT_SPI_CS_PIN    GPIO_PIN_4
+    #define LT_SPI_INSTANCE  SPI4
+    #define LT_SPI_CS_CLK_ENABLE()  __HAL_RCC_GPIOE_CLK_ENABLE()
+    #define SPIx                             SPI4
+    #define SPIx_CLK_ENABLE()                __HAL_RCC_SPI4_CLK_ENABLE()
+    #define SPIx_SCK_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOE_CLK_ENABLE()
+    #define SPIx_MISO_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOE_CLK_ENABLE()
+    #define SPIx_MOSI_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOE_CLK_ENABLE()
+    #define SPIx_NSS_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOE_CLK_ENABLE()
+    #define SPIx_FORCE_RESET()               __HAL_RCC_SPI4_FORCE_RESET()
+    #define SPIx_RELEASE_RESET()             __HAL_RCC_SPI4_RELEASE_RESET()
+    /* Definition for SPIx Pins */
+    #define SPIx_SCK_PIN                     GPIO_PIN_2
+    #define SPIx_SCK_GPIO_PORT               GPIOE
+    #define SPIx_SCK_AF                      GPIO_AF5_SPI4
+    #define SPIx_MISO_PIN                    GPIO_PIN_5
+    #define SPIx_MISO_GPIO_PORT              GPIOE
+    #define SPIx_MISO_AF                     GPIO_AF5_SPI4
+    #define SPIx_MOSI_PIN                    GPIO_PIN_6
+    #define SPIx_MOSI_GPIO_PORT              GPIOE
+    #define SPIx_MOSI_AF                     GPIO_AF5_SPI4
+  #else
+    #define LT_SPI_CS_BANK   GPIOD
+    #define LT_SPI_CS_PIN    GPIO_PIN_14
+    #define LT_SPI_INSTANCE  SPI1
+    #define LT_SPI_CS_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+    #define SPIx                             SPI1
+    #define SPIx_CLK_ENABLE()                __HAL_RCC_SPI1_CLK_ENABLE()
+    #define SPIx_SCK_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define SPIx_MISO_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define SPIx_MOSI_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define SPIx_NSS_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define SPIx_FORCE_RESET()               __HAL_RCC_SPI1_FORCE_RESET()
+    #define SPIx_RELEASE_RESET()             __HAL_RCC_SPI1_RELEASE_RESET()
+    /* Definition for SPIx Pins */
+    #define SPIx_SCK_PIN                     GPIO_PIN_5
+    #define SPIx_SCK_GPIO_PORT               GPIOA
+    #define SPIx_SCK_AF                      GPIO_AF5_SPI1
+    #define SPIx_MISO_PIN                    GPIO_PIN_6
+    #define SPIx_MISO_GPIO_PORT              GPIOA
+    #define SPIx_MISO_AF                     GPIO_AF5_SPI1
+    #define SPIx_MOSI_PIN                    GPIO_PIN_7
+    #define SPIx_MOSI_GPIO_PORT              GPIOA
+    #define SPIx_MOSI_AF                     GPIO_AF5_SPI1
+  #endif
 #endif
 /* Size of buffer */
 #define BUFFERSIZE                       (COUNTOF(aTxBuffer) - 1)
