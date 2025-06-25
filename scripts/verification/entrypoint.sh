a23a21ae7f6b599e4d38da1372f791637ae68d8f
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2025-06-25 15:36:22 +0200

    build: docker ci environment initial commit.

---
+++ scripts/verification/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+echo "Libtropic testing environment started!"
+
+cd /work
+mkdir -p build
+cd build
+
+echo "Creating Python virtual environment..."
+python3 -m venv .venv
+source .venv/bin/activate
+
+echo "Gathering Python dependencies..."
+pip3 install -r /work/repo/libtropic/scripts/test_runner/requirements.txt
+
+echo "Launching test..."
+cmake -DLT_BUILD_TESTS=1 /work/repo/NUCLEO_F439ZI
+make
+ctest --verbose -R lt_test_chip_id
+
+# TODO
+# Add NUCLEO_L432_KC and all new STM32 platforms here.
+
+echo "CTest finished! Leaving Libtropic testing environment."
