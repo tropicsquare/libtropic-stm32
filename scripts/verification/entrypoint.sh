d6f655c012f72f8b5d0a67e33356179c93c3bc23
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2025-06-26 17:58:08 +0200

    fix: removed test selection from docker entrypoint.

--- scripts/verification/entrypoint.sh
+++ scripts/verification/entrypoint.sh
@@ -17,7 +17,7 @@ pip3 install -r /work/repo/libtropic/scripts/test_runner/requirements.txt
 echo "Launching test..."
 cmake -DLT_BUILD_TESTS=1 /work/repo/NUCLEO_F439ZI
 make
-ctest --verbose -R lt_test_chip_id
+ctest --verbose 
 
 # TODO
 # Add NUCLEO_L432_KC and all new STM32 platforms here.
