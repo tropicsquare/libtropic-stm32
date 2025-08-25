c922b896ddd250fc501a913a1ca1cabd7f9e0a3f
Jan Medek <czmedexs@gmail.com>
2025-08-25 09:31:04 +0200

    feat: add clang-format and format all .c, .h files

--- NUCLEO_L432KC/Inc/stm32l4xx_it.h
+++ NUCLEO_L432KC/Inc/stm32l4xx_it.h
@@ -1,20 +1,20 @@
 /**
-  ******************************************************************************
-  * @file    SPI/SPI_FullDuplex_ComPolling/Inc/stm32l4xx_it.h
-  * @author  MCD Application Team
-  * @brief   This file contains the headers of the interrupt handlers.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2017 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    SPI/SPI_FullDuplex_ComPolling/Inc/stm32l4xx_it.h
+ * @author  MCD Application Team
+ * @brief   This file contains the headers of the interrupt handlers.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2017 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32L4xx_IT_H
@@ -45,4 +45,3 @@ void SysTick_Handler(void);
 #endif
 
 #endif /* __STM32L4xx_IT_H */
-
